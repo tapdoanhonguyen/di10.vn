aaf4d6ac88f88283854697a3ef2ea347e69b6529
Nguyễn Thanh Hoàng <hoang.nguyen@webvang.vn>
2021-10-13 16:37:28 +0700

    update

---
+++ config.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * NukeViet Content Management System
+ * @version 4.x
+ * @author VINADES.,JSC <contact@vinades.vn>
+ * @copyright (C) 2009-2021 VINADES.,JSC. All rights reserved
+ * @license GNU/GPL version 2 or any later version
+ * @see https://github.com/nukeviet The NukeViet CMS GitHub project
+ */
+
+if (!defined('NV_MAINFILE')) {
+    exit('Stop!!!');
+}
+
+$db_config['dbhost'] = 'localhost';
+$db_config['dbport'] = '';
+$db_config['dbname'] = 'di10_home';
+$db_config['dbsystem'] = 'di10_home';
+$db_config['dbuname'] = 'di10_home';
+$db_config['dbpass'] = 'Ht159753123@';
+$db_config['dbtype'] = 'mysql';
+$db_config['collation'] = 'utf8mb4_unicode_ci';
+$db_config['charset'] = 'utf8mb4';
+$db_config['persistent'] = false;
+$db_config['prefix'] = 'nv4';
+
+$global_config['site_domain'] = '';
+$global_config['name_show'] = 0;
+$global_config['idsite'] = 0;
+$global_config['sitekey'] = '1f75f36f5e58ec8260d2cffbd152d622';// Do not change sitekey!
+$global_config['hashprefix'] = '{SSHA512}';
+$global_config['cached'] = 'files';
+$global_config['session_handler'] = 'files';
+$global_config['extension_setup'] = 3; // 0: No, 1: Upload, 2: NukeViet Store, 3: Upload + NukeViet Store
+// Readmore: https://wiki.nukeviet.vn/nukeviet4:advanced_setting:file_config
